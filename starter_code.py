a4853f66e9bb10190708b2c00eafbb40daf5a002
munirmalik <munirmalik304@gmail.com>
2018-04-04 18:29:46 +0100

    Update starter_code.py

--- starter_code.py
+++ starter_code.py
@@ -5,25 +5,25 @@ Colomb-ia
 
 """
 
-#Importar librerías
+#Importar librerías/Import libraries
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 from time import time
 #features_train, features_test, labels_train, labels_test = preprocess()
 
-#Leer datos
+#Leer datos/Read data
 train = pd.read_csv("data/iris_train.csv")
 test = pd.read_csv("data/iris_test.csv")
 
-#Make the data sklearn-friendly
+#Tratamiento de datos/Data processing
 features_train = train[['Sepal length','Sepal width','Petal length','Petal width']]
 labels_train =  train['Class']
 features_test= test[['Sepal length','Sepal width','Petal length','Petal width']]
 labels_test= test['Class']
 
 
-
+#El función/The function
 def randomForest(features_train,labels_train):
 	clf=RandomForestClassifier(n_estimators=100,min_samples_leaf=20,max_features="auto")
 	clf=clf.fit(features_train,labels_train)
